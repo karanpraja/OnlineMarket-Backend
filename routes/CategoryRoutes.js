12d5fb9a4a441f50dca015ca3e3258a5e817166c
Karan <karanpraja902@gmail.com>
2024-02-28 12:40:28 +0530

    Vercel deployment

--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -2,5 +2,5 @@ const express=require('express')
 const { CreateCategory, fetchCategory } = require('../controller/CategoryController')
 const router=express.Router()
 
-router.post('/category',CreateCategory).get('/categories',fetchCategory)
+router.post('/categories/:id',CreateCategory).get('/categories',fetchCategory)
 exports.router=router
